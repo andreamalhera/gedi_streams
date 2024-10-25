1f8746981874b830383a583fac94dee19ac62790
Andrea Maldonado <>
2024-10-25 18:14:38 +0200

    Initial commit from GEDI

---
+++ main.py
@@ -0,0 +1,12 @@
+import config
+from datetime import datetime as dt
+from gedi.run import gedi, run
+from utils.default_argparse import ArgParser
+from utils.param_keys import *
+
+if __name__=='__main__':
+    start_gedi = dt.now()
+    print(f'INFO: GEDI starting {start_gedi}')
+    args = ArgParser().parse('GEDI main')
+    gedi(args.alg_params_json)
+    print(f'SUCCESS: GEDI took {dt.now()-start_gedi} sec.')
