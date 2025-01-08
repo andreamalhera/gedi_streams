c2f9a49ae5ca0a7d6791f5eea2aeb80445bf0430
Andrea Maldonado <>
2025-01-08 18:20:38 +0100

    Imports parent class from FEEED

--- gedi_streams/features/stream_features.py
+++ gedi_streams/features/stream_features.py
@@ -2,7 +2,7 @@ import inspect
 import numpy as np
 import re
 
-from gedi_streams.features.feature import Feature
+from feeed.feature import Feature
 from gedi_streams.features.memory import ComputedFeatureMemory
 from gedi_streams.utils.io_helpers import list_classes_in_file
 from pm4py.objects.log.obj import EventLog
@@ -30,6 +30,16 @@ class StreamFeature(Feature):
             self.feature_names = [*self.available_class_methods.keys()]
         else:
             self.feature_names = feature_names
+
+    def extract(self, log, memory: ComputedFeatureMemory):
+        feature_names=self.feature_names
+
+        output = {}
+        for feature_name in feature_names:
+            feature_fn = self.available_class_methods[feature_name]
+            feature_value = feature_fn(log, memory)
+            output[f"{feature_name}"] = feature_value
+        return output
 
 class SimpleStreamStats(StreamFeature):
     def __init__(self, feature_names='simple_stream_stats', memory=None):
