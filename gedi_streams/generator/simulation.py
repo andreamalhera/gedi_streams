a9b08e4d21900db048653aebb19ac2a00cc321e4
chimenkamp@gmail.com <chimenkamp@gmail.com>
2025-03-12 10:57:28 +0100

    Combine GEDI and DEF code

--- gedi_streams/generator/simulation.py
+++ gedi_streams/generator/simulation.py
@@ -1,11 +1,17 @@
 import os
 import sys
+from pathlib import Path
+from queue import Queue
+from typing import List, Optional
 
-# Add the submodule to sys.path
+from distributed_event_factory.core.datasource import GenericDataSource
+from gedi_streams.def_configurations.sink.gedi_sink import GEDIAdapter
+from DistributedEventFactory.distributed_event_factory.event_factory import EventFactory
+
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 submodule_path = os.path.join(project_root, "DistributedEventFactory")
 sys.path.append(submodule_path)
-from DistributedEventFactory.distributed_event_factory.event_factory import EventFactory
+
 
 class QueueOutput:
     def __init__(self, queue):
@@ -18,15 +24,21 @@ class QueueOutput:
         # Flush is needed to avoid warnings about buffered output
         pass
 
-def play_DEFact(model=None, config=None, queue=None):
+
+
+def play_DEFact(model=None, config=None, queue: Optional[Queue]=None, print_events: bool=True):
     if queue is None:
-        ##TODO: Play out a DEFact stream here.
-        pass
-    else:
-        sys.stdout = QueueOutput(queue)
-        event_factory = EventFactory()
-        (event_factory
-        .add_directory("DistributedEventFactory/config/datasource/assemblyline/")
-        .add_file("DistributedEventFactory/config/simulation/stream.yaml")
-        .add_file("DistributedEventFactory/config/sink/console-sink.yaml")
-        ).run()
+        print("Queue is None")
+        return None
+    event_factory = EventFactory()
+
+    data_sources: List[GenericDataSource] = []
+    # data_source_ref: List[str] = list(map(lambda x: x.name, data_sources))
+    data_source_ref = ["GoodsDelivery", "MaterialPreparation", "AssemblyLineSetup", "Assembling", "QualityControl",
+                       "Packaging", "Shipping"]
+
+    event_factory.add_sink("gedi-sink", GEDIAdapter("gedi-sink", data_source_ref, queue, disable_console_print=not print_events))
+    (event_factory
+     .add_directory(f"{submodule_path}/config/datasource/assemblyline")
+     .add_file(f"{submodule_path}/config/simulation/stream.yaml")
+     ).run()
